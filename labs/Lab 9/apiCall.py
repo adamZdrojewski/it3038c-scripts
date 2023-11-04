51382cfe7da66e205f7d433742cd8779df59e7af
Adam Zdrojewski <46827462+adamZdrojewski@users.noreply.github.com>
2023-11-04 13:38:41 -0400

    Adds lab 9

---
+++ labs/Lab 9/apiCall.py
@@ -0,0 +1,9 @@
+import json 
+import requests
+ 
+r = requests.get("http://localhost:3000") 
+data=r.json() 
+count = 0
+while count < (len(data)):
+    print(data[count]["name"] + " is " + data[count]["color"])
+    count = count + 1
